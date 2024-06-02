0b392f6326a36bfe738947f490d32c1669761250
Marcelo <qw3xvf@devdox.net>
2024-06-02 13:40:58 +0900

    remove macos related files

--- macos.sh
+++
@@ -1,8 +0,0 @@
-#
-# APPLE SILICON
-#
-
-# If you're on Apple Silicon macOS 13.x.x, or 12.x.x
-# Requires Filesystem Protections, Debugging Restrictions and NVRAM Protection to be disabled
-# (printed warning can be safely ignored)
-csrutil disable --with kext --with dtrace --with basesystem
