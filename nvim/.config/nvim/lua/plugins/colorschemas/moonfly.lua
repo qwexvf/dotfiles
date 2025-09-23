2eb1c148571821dddba9ad3a20b61095f632908f
qwexvf <qw3xvf@devdox.net>
2025-09-23 05:06:51 +0000

    update

--- nvim/.config/nvim/lua/plugins/colorschemas/moonfly.lua
+++ nvim/.config/nvim/lua/plugins/colorschemas/moonfly.lua
@@ -4,6 +4,17 @@ return {
   lazy = false,
   priority = 1000,
   config = function()
+    -- Configure moonfly options before setting colorscheme
+    vim.g.moonflyCursorColor = true
+    vim.g.moonflyItalics = true
+    vim.g.moonflyNormalFloat = true
+    vim.g.moonflyTerminalColors = true
+    vim.g.moonflyTransparent = false
+    vim.g.moonflyUndercurls = true
+    vim.g.moonflyUnderlineMatchParen = true
+    vim.g.moonflyVirtualTextColor = true
+    
+    -- Set the colorscheme
     vim.cmd.colorscheme "moonfly"
   end,
 }
--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,17 +1,41 @@
--- Pull in the wezterm API
 local wezterm = require 'wezterm'
-
--- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
+-- Colors (Moonfly theme matching Alacritty)
+config.colors = {
+  foreground = '#bdbdbd',
+  background = '#080808',
+  cursor_bg = '#8e8e8e',
+  cursor_fg = '#080808',
+  cursor_border = '#8e8e8e',
+  selection_fg = '#080808',
+  selection_bg = '#b2ceee',
+  
+  ansi = {
+    '#323437', '#ff5d5d', '#8cc85f', '#e3c78a',
+    '#80a0ff', '#cf87e8', '#79dac8', '#c6c6c6',
+  },
+  brights = {
+    '#949494', '#ff5189', '#36c692', '#c6c684',
+    '#74b2ff', '#ae81ff', '#85dc85', '#e4e4e4',
+  },
+}
 
--- For example, changing the color scheme:
-config.color_scheme = 'rose-pine'
+-- Font (matching Alacritty size)
+config.font = wezterm.font 'ZedMono Nerd Font'
+config.font_size = 12
+
+-- Window (matching Alacritty settings)
+config.window_decorations = 'NONE'
+config.window_padding = { left = 5, right = 5, top = 5, bottom = 5 }
+
+-- Disable all extra features for simplicity
 config.enable_tab_bar = false
+config.enable_scroll_bar = false
+config.use_fancy_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
+config.window_close_confirmation = 'NeverPrompt'
+config.audible_bell = 'Disabled'
+config.visual_bell = { fade_in_function = 'EaseIn', fade_in_duration_ms = 0 }
 
-config.font = wezterm.font 'Iosevka Nerd Font'
-config.font_size = 16
-
--- and finally, return the configuration to wezterm
 return config
